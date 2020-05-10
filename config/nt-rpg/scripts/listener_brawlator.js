99b80a9de947b856ea6b46a4301d88fa815569b6
hellbringer616 <m.johnsondelta@gmail.com>
2020-05-10 08:22:13 -0400

    Update listener_brawlator.js
    test4

--- config/nt-rpg/scripts/listener_brawlator.js
+++ config/nt-rpg/scripts/listener_brawlator.js
@@ -1,5 +1,5 @@
 registerEventListener({
-  type: Java.type('org.spongepowered.api.event.item.inventory.DropItemEvent'),
+  type: Java.type('org.spongepowered.api.event.entity.AttackEntityEvent'),
   consumer: new java.util.function.Consumer( function(event) {
     log("Im Javascript event handler");
   })
