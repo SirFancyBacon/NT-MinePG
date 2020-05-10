16d60e1d6b0c6d07fe052a2e04a0a207c444e64c
hellbringer616 <m.johnsondelta@gmail.com>
2020-05-10 08:34:57 -0400

    Update listener_brawlator.js
    got it to load; now for other things

--- config/nt-rpg/scripts/listener_brawlator.js
+++ config/nt-rpg/scripts/listener_brawlator.js
@@ -1,6 +1,6 @@
 registerEventListener({
   type: Java.type('org.spongepowered.api.event.entity.AttackEntityEvent'),
   consumer: new java.util.function.Consumer( function(event) {
-    log("Im Javascript event handler");
+    org.spongepowered.api.Sponge.getCommandManager().process(Sponge.getServer().getConsole(), "nadmin exp hellbringer616 10 PVE");
   })
 });
