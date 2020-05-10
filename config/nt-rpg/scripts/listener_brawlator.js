ae16f8998daa5c399d6390ec0a9cd90a0ba10bcc
hellbringer616 <m.johnsondelta@gmail.com>
2020-05-10 08:38:12 -0400

    Update listener_brawlator.js

--- config/nt-rpg/scripts/listener_brawlator.js
+++ config/nt-rpg/scripts/listener_brawlator.js
@@ -1,6 +1,7 @@
 registerEventListener({
   type: Java.type('org.spongepowered.api.event.entity.AttackEntityEvent'),
   consumer: new java.util.function.Consumer( function(event) {
-    org.spongepowered.api.Sponge.getCommandManager().process(Sponge.getServer().getConsole(), "nadmin exp hellbringer616 10 PVE");
+    var Sponge = Java.type("org.spongepowered.api.Sponge")
+    Sponge.getCommandManager().process(Sponge.getServer().getConsole(), "nadmin exp hellbringer616 10 PVE");
   })
 });
