df552c739de6eb677833e2496b5e77f1fd2fd271
hellbringer616 <m.johnsondelta@gmail.com>
2020-05-11 17:59:33 -0400

    Update listener_brawlator.js
    attempt 3

--- config/nt-rpg/scripts/listener_brawlator.js
+++ config/nt-rpg/scripts/listener_brawlator.js
@@ -2,9 +2,10 @@ registerEventListener({
   type: Java.type('com.onaple.brawlator.events.BrawlatorEntityDiedEvent'),
   consumer: new java.util.function.Consumer( function(event) {
     var Sponge = Java.type("org.spongepowered.api.Sponge");
+    var BrawlatorData = Java.type('com.onaple.brawlator.data.manipulators.MonsterLootManipulator')
     var playerName = event.getPlayer().getName();
     var brawlatorMob = event.getEntity();
-    var xp = brawlatorMob.get(Java.type('com.onaple.brawlator.data.manipulator.MonsterLootManipulator'));
+    var xp = brawlatorMob.get(BrawlatorData);
       Sponge.getCommandManager().process(Sponge.getServer().getConsole(), "nadmin exp " + playerName + " " + xp +" PVE");
   })
 });
