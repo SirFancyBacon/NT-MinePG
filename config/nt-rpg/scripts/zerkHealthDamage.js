869c059e74660de54d3bbf47a9e879c1f781b5bb
hellbringer616 <m.johnsondelta@gmail.com>
2020-07-02 13:46:39 -0400

    Update zerkHealthDamage.js
    correct based on feedback from NT

--- config/nt-rpg/scripts/zerkHealthDamage.js
+++ config/nt-rpg/scripts/zerkHealthDamage.js
@@ -1,10 +1,12 @@
 registerSkillHandler('ntminepg:zerkHealthDamage',{
-  onCast: function(character, context) {
-    var totalDuration = param("duration", context); //creates "duration" and "duration_levelbonus" configuration options in the skilltree file
-    var totalMaxMulti = param("maxboot", context);
-    var EffectBase = Java.type("cz.neumimto.rpg.api.effects.EffectBase") //creates an effect (also called buff)
-    var zerkRage = Java.extend(EffectBase, {}); //defines the "buff" applied to the player character
+  var EffectBase = Java.type("cz.neumimto.rpg.api.effects.EffectBase"); //creates an effect (also called buff)
+  var MyEffect = Java.extend(EffectBase, {}); //defines the "buff" applied to the player character
 
+  onCast: function(character, context) {
+    var effect = new MyEffect("zerkRage", character); //creates an effect on the character called zerkRage
+    var totalDuration = param("duration", context); //creates "duration" and "duration_levelbonus" configuration options in the skilltree file
+    effect.setDuration(totalDuration);
+    
     return SkillResult.OK;
   }
 )}
