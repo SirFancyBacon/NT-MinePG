fc851968a8d5022962edadda682b0441e659734c
hellbringer616 <m.johnsondelta@gmail.com>
2020-08-23 17:24:25 -0400

    Update listener_brawlatorGiveXp.js
    Fix XP gain maybe

--- config/nt-rpg/scripts/listener_brawlatorGiveXp.js
+++ config/nt-rpg/scripts/listener_brawlatorGiveXp.js
@@ -5,9 +5,10 @@ registerEventListener({
   consumer: new java.util.function.Consumer( function(event) {
     var Sponge = Java.type('org.spongepowered.api.Sponge'); //loads Sponge
     var BrawlatorKeys = Java.type('com.onaple.brawlator.BrawlatorKeys'); //loads BrawlatorKeys from Brawlator plugin
+    var ThirdPartyKey = Java.type('com.onaple.brawlator.plugins.ExperienceThirdParty');
     var playerName = event.getPlayer().getName(); //gets Player Name of player that killed the mob
     var brawlatorMob = event.getEntity(); //gets Brawlator Mob killed
-    var xp = brawlatorMob.get(BrawlatorKeys.EXPERIENCE); //gets experience key from config of killed Mob; optional
+    var xp = brawlatorMob.get(ThirdPartyKey.EXPERIENCE); //gets experience key from config of killed Mob; optional
       //Checks if XP value is present on Mob, if yes run command.
       if (xp.ispresent()){
         Sponge.getCommandManager().process(Sponge.getServer().getConsole(), "nadmin exp " + playerName + " " + xp.get() +" PVE"); //Gives player XP to all classes with PVE source.
